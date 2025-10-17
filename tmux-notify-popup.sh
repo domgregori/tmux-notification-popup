27f941cd31ff502425101c67f99a3087296c621a
domgregori <dom.gregori@gmail.com>
2025-10-17 02:20:52 -0400

    Icons left/right

--- tmux-notify-popup.sh
+++ tmux-notify-popup.sh
@@ -6,6 +6,8 @@
 # -c        center text (default; use -C to disable)
 # -T COLOR  text color name or #RRGGBB
 # -B COLOR  background color name or #RRGGBB
+# -i ICON   emoji or string placed top-left of popup
+# -I POS    icon position: left (default) or right
 # -t TITLE  popup title (default "Notification")
 # stdin     message content (used when -m not supplied)
 
@@ -19,10 +21,11 @@ CENTER=true
 TITLE=""
 TEXT_COLOR=""
 BG_COLOR=""
+ICON=""
+ICON_POSITION="left"
 MSG_SET=false
 STYLE_PREFIX=""
 STYLE_SUFFIX=""
-SHOW_CURSOR=$'\e[?25h'
 
 pick_color() {
   if [[ $1 =~ ^#?[0-9A-Fa-f]{6}$ ]]; then
@@ -59,7 +62,40 @@ hex_to_rgb() {
   printf "%d;%d;%d" "0x${h:0:2}" "0x${h:2:2}" "0x${h:4:2}"
 }
 
-while getopts ":m:l:d:cCT:B:t:" opt; do
+setup_icon_padding() {
+  ICON_PAD_WIDTH=0
+  ICON_PREFIX_FIRST=""
+  ICON_PREFIX_OTHER=""
+  ICON_SUFFIX_FIRST=""
+  ICON_SUFFIX_OTHER=""
+
+  [[ -z $ICON ]] && return
+
+  ICON_WIDTH=2
+  ICON_PAD_WIDTH=$((ICON_WIDTH + 1))
+
+  local icon_space icon_pad space_before_icon
+  icon_space=$(printf "%*s" "$ICON_PAD_WIDTH" "")
+  space_before_icon=$(printf "%*s" "$((ICON_PAD_WIDTH - ICON_WIDTH))" "")
+  icon_pad="${ICON} "
+
+  case "$ICON_POSITION" in
+  right)
+    ICON_PREFIX_FIRST=""
+    ICON_PREFIX_OTHER=""
+    ICON_SUFFIX_FIRST="${space_before_icon}${ICON}"
+    ICON_SUFFIX_OTHER="$icon_space"
+    ;;
+  *)
+    ICON_PREFIX_FIRST="$icon_pad"
+    ICON_PREFIX_OTHER="$icon_space"
+    ICON_SUFFIX_FIRST=""
+    ICON_SUFFIX_OTHER=""
+    ;;
+  esac
+}
+
+while getopts ":m:l:d:cCT:B:t:i:I:" opt; do
   case "$opt" in
   m)
     MSG="$OPTARG"
@@ -72,6 +108,8 @@ while getopts ":m:l:d:cCT:B:t:" opt; do
   T) TEXT_COLOR="$OPTARG" ;;
   B) BG_COLOR="$OPTARG" ;;
   t) TITLE="$OPTARG" ;;
+  i) ICON="$OPTARG" ;;
+  I) ICON_POSITION="$OPTARG" ;;
   :)
     printf 'Option -%s requires an argument\n' "$OPTARG" >&2
     exit 1
@@ -95,6 +133,14 @@ if ! CLIENT_WIDTH=$(tmux display-message -p '#{client_width}' 2>/dev/null); then
   printf 'tmux-notify-popup: must be run inside an active tmux client\n' >&2
   exit 1
 fi
+
+case "$ICON_POSITION" in
+left | right) ;;
+*)
+  printf 'Invalid icon position: %s (use "left" or "right")\n' "$ICON_POSITION" >&2
+  exit 1
+  ;;
+esac
 
 if [[ $LEN_SPEC =~ ^([0-9]+)%$ ]]; then
   PERCENT_VALUE=${BASH_REMATCH[1]}
@@ -123,23 +169,42 @@ fi
 
 HIDE_CURSOR=$(printf '\e[?25l')
 
+setup_icon_padding
+
+TOTAL_WIDTH=$((LEN + ICON_PAD_WIDTH))
+
 # Wrap and center lines
 mapfile -t RAW_LINES < <(printf "%s" "$MSG" | fold -s -w "$LEN")
 if ((${#RAW_LINES[@]} == 0)); then
   RAW_LINES=(" ")
 fi
 LINES=()
-for line in "${RAW_LINES[@]}"; do
+for idx in "${!RAW_LINES[@]}"; do
+  line="${RAW_LINES[$idx]}"
   if $CENTER; then
-    pad=$(((LEN - ${#line}) / 2))
+    line_len=${#line}
+    pad=$(((TOTAL_WIDTH - line_len) / 2 - ICON_PAD_WIDTH))
     ((pad < 0)) && pad=0
     line="$(printf "%*s%s" "$pad" "" "$line")"
   fi
-  LINES+=("${HIDE_CURSOR}${STYLE_PREFIX}$(printf "%-${LEN}s" "$line")${STYLE_SUFFIX}")
+  line="$(printf "%-${LEN}s" "$line")"
+  if [[ -n $ICON ]]; then
+    if ((idx == 0)); then
+      prefix="$ICON_PREFIX_FIRST"
+      suffix="$ICON_SUFFIX_FIRST"
+    else
+      prefix="$ICON_PREFIX_OTHER"
+      suffix="$ICON_SUFFIX_OTHER"
+    fi
+  else
+    prefix=""
+    suffix=""
+  fi
+  LINES+=("${HIDE_CURSOR}${STYLE_PREFIX}${prefix}${line}${suffix}${STYLE_SUFFIX}")
 done
 
 # Dimensions (tiny padding)
-WIDTH=$((LEN + 2))
+WIDTH=$((LEN + ICON_PAD_WIDTH + 2))
 HEIGHT=$((${#LINES[@]} + 2))
 
 X_POS=$((CLIENT_WIDTH - WIDTH - 1))
